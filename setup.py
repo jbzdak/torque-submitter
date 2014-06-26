dec8cad89420747d48f126c62f82f8f9443ce66f
Jacek Bzdak <jbzdak@gmail.com>
2014-06-26 23:24:05 +0200

    setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='torque-submit',
+    version='0.0.1',
+    packages=['torqsubmit'],
+    url='',
+    license='Apache 2.0',
+    author='Jacek Bzdak',
+    author_email='jbzdak@gmail.com',
+    description='Python script to submit torque jobs in a very hackish way'
+)
